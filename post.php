8ee5f435df82f6ecc3dd73d5ac9b71015670d454
jyrgenhallast <jurgen.hallast@khk.ee>
2015-12-14 03:09:36 +0200

    4. API 2 [Delivers #107559426]

---
+++ post.php
@@ -0,0 +1,6 @@
+<?php
+if (!empty($_POST['aadress'])) {
+    $file = fopen("aadressid.txt", "a") or die("Unable to open file!");
+    fwrite($file, $_POST['aadress'] . "\n");
+    }
+header('Location: yl4.html');
